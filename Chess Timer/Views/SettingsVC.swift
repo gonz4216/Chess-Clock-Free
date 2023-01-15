264c3bb2cf1f8b671d9aa31d18a0447c96651c96
gonz4216 <ethgon19@gmail.com>
2023-01-15 17:14:09 -0500

    Delegates finished

--- Chess Timer/Views/SettingsVC.swift
+++ Chess Timer/Views/SettingsVC.swift
@@ -7,25 +7,74 @@
 
 import UIKit
 
+protocol SettingsVCDelegate {
+    func didChangeMin(min: inout Int)
+}
+
 class SettingsVC: UIViewController {
 
+    var delegate: SettingsVCDelegate?
     
    
- 
+    @IBOutlet var MinuteLabel: UILabel!
+  
+    var minCount: Int = Int()
     
     
  
     override func viewDidLoad() {
         
         super.viewDidLoad()
-       
+        
       
                
      }
     
-    
-   
-  
-    
+    func plusCount() {
+        minCount += 1
+        return MinuteLabel.text = String("\(minCount) Minutes")
 
+       
+    }
+    
+    func minusCount() {
+        if minCount < 1 {
+            return
+        }
+       minCount -= 1
+        return MinuteLabel.text = String("\(minCount) Minutes")
+
+    }
+    
+    
+    @IBAction func plusTimeTapped(_ sender: UIButton) {
+        plusCount()
+        delegate?.didChangeMin(min: &minCount)
+        TimerVC().didResetDelegate = self
+        func didReset(resetMin: Int) {
+           minCount = resetMin
+        }
+        MinuteLabel.text = String("\(minCount) Minutes")
+
+        
+    }
+    
+  
+    @IBAction func MinTimeTapped(_ sender: UIButton) {
+        minusCount()
+        
+        MinuteLabel.text = String("\(minCount) Minutes")
+      
+    }
+    
+     
+    
+}
+extension SettingsVC: TimerVCDelegate {
+    func didReset(resetMin: Int) {
+        
+       minCount = 0
+    }
+    
+    
 }
--- Chess Timer/Views/TimerVC.swift
+++ Chess Timer/Views/TimerVC.swift
@@ -6,16 +6,20 @@
 //
 
 import UIKit
-
+protocol TimerVCDelegate {
+    func didReset(resetMin: Int)
+}
 class TimerVC: UIViewController {
     
 
-   
+    var didResetDelegate: TimerVCDelegate?
  
     var timer: Timer = Timer()
-    var count: Int = 0
+    var count: Int = Int()
     var timerCounting = false
   
+    
+    
     
     
  
@@ -32,10 +36,22 @@ class TimerVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         rotate()
-        pauseBtn.setTitle("test", for: .normal)
-      
+        
+        
     }
  
+   
+    
+    
+    
+    
+    
+    @IBAction func settingsBtnTapped(_ sender: UIButton) {
+        let settingsVC =  self.storyboard?.instantiateViewController(withIdentifier: "SettingsVC") as! SettingsVC;
+        self.present(settingsVC, animated: true)
+        settingsVC.delegate = self
+    }
+    
     private func rotate() {
         topTap.transform = CGAffineTransform(rotationAngle: .pi)
         topTime.transform = CGAffineTransform(rotationAngle: .pi)
@@ -45,21 +61,33 @@ class TimerVC: UIViewController {
         if(timerCounting) {
             timerCounting = false
             timer.invalidate()
-            pauseBtn.setTitle("START", for: .normal)
+            pauseBtn.setImage(UIImage(named: "pause"), for: .normal)
+          
         } else {
             timerCounting = true
-            pauseBtn.setTitle("STOP", for: .normal)
+            pauseBtn.setImage(UIImage(named: "play"), for: .normal)
+          
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
         }
+        
+       
        
     }
-    
+   
     
     @objc func timerCounter() -> Void {
-            count += 1
+        if count <= 0 {
+            count = 1
+        
+        } else {
+            count -= 1
         let time = secondsToHoursMinSecs(seconds: count)
         let timeString = makeTimeString(min: time.0, seconds: time.1)
         greenTeamLabel.text = timeString
+        }
+          
+        
+       
     }
     
     func secondsToHoursMinSecs(seconds: Int) -> (Int, Int) {
@@ -77,15 +105,19 @@ class TimerVC: UIViewController {
     
     
     @IBAction func resetTapped(_ sender: UIButton) {
-        
+        timer.invalidate()
         let alert = UIAlertController(title: "Restart Timer?", message: "Are you sure you would like to restart timer", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
             //do nothing
         }))
         alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
+            self.didResetDelegate?.didReset(resetMin: self.count)
             self.count = 0
             self.timer.invalidate()
             self.greenTeamLabel.text = self.makeTimeString(min: 0, seconds: 0)
+            
+            
+             
             
         }))
         
@@ -96,3 +128,15 @@ class TimerVC: UIViewController {
     
 }
 
+extension TimerVC: SettingsVCDelegate {
+    func didChangeMin(min: inout Int) {
+        count = min * 60
+        self.greenTeamLabel.text = self.makeTimeString(min: min, seconds: 0)
+    }
+    
+   
+    
+     
+    
+    
+}
